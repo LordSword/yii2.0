62d4be09f21033be4879f54a80d5ac7d4a7c3b62
pengshuai <psmingyu@vip.qq.com>
2017-12-19 16:53:34 +0800

    生产环境与开发环境的分离

---
+++ environments/prod/common/config/main.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=rm-wz96w94e22801h88pbo.mysql.rds.aliyuncs.com;dbname=advanced',
+            'username' => 'ps_study',
+            'password' => 'bgdWYsbwr180',
+            'charset' => 'utf8',
+        ],
+    ],
+];
