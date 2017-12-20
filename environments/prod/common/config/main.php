e6192dda195f4d600f0964107b0188fbea9dc01d
pengshuai <psmingyu@vip.qq.com>
2017-12-20 13:55:57 +0800

    更改配置

---
+++ environments/prod/common/config/main.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=;dbname=',
+            'username' => '',
+            'password' => '',
+            'charset' => 'utf8',
+        ],
+    ],
+];
